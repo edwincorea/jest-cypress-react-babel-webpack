8fe4f23d12f56c86e16a92eb8c24572c83bb0adc
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-11-11 16:35:12 -0600

    Adjusting merge.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.module\\.css$': 'identity-obj-proxy',
-    '\\.css$': require.resolve('./test/style-mock.js'),
-  },
-  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        '\\.module\\.css$': 'identity-obj-proxy',
+        '\\.css$': require.resolve('./test/style-mock.js'),
+    },
+    setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
 }
