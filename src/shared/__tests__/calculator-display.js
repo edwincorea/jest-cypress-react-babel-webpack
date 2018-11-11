77286cf04b029f4a2bb3601a2007eba0930b47e6
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-11-11 15:32:53 -0600

    Adjusting merge.

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -4,6 +4,6 @@ import {render} from 'react-testing-library'
 import CalculatorDisplay from '../calculator-display'
 
 test('mounts', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
-  expect(container.firstChild).toMatchSnapshot()
+    const {container} = render(<CalculatorDisplay value="0" />)
+    expect(container.firstChild).toMatchSnapshot()
 })
